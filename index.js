23cd19c8f7c0bf45f6fb78ba63cf1ea03522fa88
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-04-10 19:07:06 -0400

    initial JS code added

---
+++ index.js
@@ -0,0 +1,33 @@
+const numsAndChars = /[^a-zA-Z0-9\s]/;
+let about = document.getElementById('aboutModal')
+let aboutBtn = document.getElementById('abtBtn')
+let aboutSpan = document.querySelector('.abtClose');
+let rules = document.getElementById('rulesModal')
+let rulesBtn = document.getElementById('rulesBtn')
+let rulesSpan = document.querySelector('.rulesClose')
+
+//opens modal
+aboutBtn.onclick = function(){
+    about.style.display = 'block';
+}
+
+rulesBtn.onclick = function(){
+    rules.style.display = 'block';
+}
+//close modal
+aboutSpan.onclick = function(){
+    about.style.display = 'none';
+}
+
+rulesSpan.onclick = function(){
+    rules.style.display = 'none';
+}
+
+//click anywhere to close modal
+window.onclick = function(event){
+    if(event.target === about || event.target === rules || event.target === contact){
+        about.style.display = 'none'
+        rules.style.display = 'none'
+        contact.style.display = 'none'
+    }
+}
